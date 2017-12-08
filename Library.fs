6c342b5ff4dd5a40ca9cebf9457259393adbb73f
Thomas Gebert <thomas@gebert.sexy>
2017-12-08 17:39:21 -0500

    Fix it so we handle the ending newline

--- Library.fs
+++ Library.fs
@@ -22,7 +22,7 @@ module CSVParse =
                       | c   -> c
 
 
-  let nonQuotedCellChar delim = escapeChar <|> (noneOf (delim + "\r\n"))
+  let nonQuotedCellChar delim = escapeChar <|> (noneOf (delim + System.Environment.NewLine))
   let cellChar = escapeChar <|> (noneOf "\"")
 
   let cell delim = between (chr '\"') (chr '\"') (manyChars cellChar) <|> manyChars (nonQuotedCellChar delim)
@@ -31,13 +31,13 @@ module CSVParse =
   let csv delim = sepBy (row delim) newline .>> eof
   let commaCsv = csv ","
 
-  let stripEmpty ls = Seq.filter (fun (row:'a list) -> row.Length <> 0) ls
+  let stripEmpty ls = Seq.filter (fun (row:'a list) -> (row.Length <> 0) && (row <> [""]) ) ls
 
   let ParseCsv s delim  =
-    let res = run (csv delim) s in
-      match res with
-      | Success (rows, _, _) -> { IsSuccess = true; ErrorMsg = "Ok"; Result = stripEmpty rows }
-      | Failure (s, _, _) -> { IsSuccess = false; ErrorMsg = s; Result = [[]] |> Seq.ofList}
+      let res = run (csv delim) s in
+          match res with
+          | Success (rows, _, _) -> { IsSuccess = true; ErrorMsg = "Ok"; Result = stripEmpty rows }
+          | Failure (s, _, _) -> { IsSuccess = false; ErrorMsg = s; Result = [[]] |> Seq.ofList}
   let ParseCsvStream path delim =
       let res = runParserOnFile (csv delim) () path (System.Text.Encoding.UTF8) 
       match res with
